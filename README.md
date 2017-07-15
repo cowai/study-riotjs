9d84f0b65c6a3de307c80ef38ed5e2eae0a59d1c
cowai <iwaoyuki1205@gmail.com>
2017-07-16 01:42:01 +0900

    test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # study-riotjs
 study Riot.js
+
+とりあえず試してみたいって方のための Riot.js 入門 | phiary
+http://phiary.me/riotjs-tutorial-try/
---
+++ sample.js
@@ -0,0 +1,9 @@
+/*
+ * Runstant
+ * 思いたったらすぐ開発. プログラミングに革命を...
+ */
+
+window.onload = function() {
+    // TODO: write code
+    // console.log("Hello, runstant!");
+};
